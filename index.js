54787e0a12c80fa15345e5a0dd46643ec6a33617
Nick <nickgividen@gmail.com>
2021-06-12 20:21:15 -0500

    made page more dynamic and functional

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+/* Mobile Nav function */
+
 function showMenu() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var menu = document.getElementById("links");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("shown");
   } else {
-    x.style.display = "block";
+    menu.classList.remove("shown");
+    menu.classList.add("hidden");
   }
 }
+
+var burger = document.getElementById("hamburger");
+
+burger.addEventListener("click", showMenu);
+
+/* function to fade in sections */
+
+let sections = document.querySelectorAll(".content__container");
+window.onscroll = function fadeIn() {
+  sections.forEach((section) => {
+    let sectionSize = section.getBoundingClientRect();
+    let topPart = sectionSize.top;
+    if (window.scrollY > topPart) {
+      section.style.opacity = "1";
+      section.style.transform = "translateX(0)";
+      section.style.transition = "600ms ease-in-out";
+    } else {
+      section.style.opacity = "0";
+      section.style.transform = "translateX(-40px)";
+      section.style.transition = "600ms ease-in-out";
+    }
+  });
+};
